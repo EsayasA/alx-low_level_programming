ccf892bcdc8fc054303e7243baf7801bfeae19e6
EsayasA <esayasaregawi29@gmail.com>
2023-06-16 14:08:50 -0700

    no11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int ch;
+
+for (ch = '0'; ch <= '9'; ch++)
+{
+for (ch1 = ch + 1; ch <= '9'; ch++)
+{
+putchar(ch);
+putchar(ch1);
+putchar(',');
+putchar(' ');
+}}
+putchar('\n');
+
+return (0);
+}
+
