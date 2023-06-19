f12634662ec04f686be162acb15d4d80c17c2b89
EsayasA <esayasaregawi29@gmail.com>
2023-06-19 10:00:33 -0700

    no11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,17 +8,17 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j < '10' ; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			for (k = j + 1; k < '10'; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				if (i != j && j != k)
 				{
-				putchar(i);
-				putchar(j);
-				putchar(k);
+				putchar('i');
+				putchar('j');
+				putchar('k');
 				}
 				if (i != 7)
 				{
@@ -29,3 +29,4 @@ int main(void)
 
 	return (0);
 }
+
