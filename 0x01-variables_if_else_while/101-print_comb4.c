a133ca1686c9f6c6a57b9fa9d8bd9c38c0b35f7c
EsayasA <esayasaregawi29@gmail.com>
2023-06-19 10:11:53 -0700

    no11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = i + 1; j < '10'; j++)
+		for (j = i + 1; j <= '9'; j++)
 		{
-			for (k = j + 1; k < '10'; k++)
+			for (k = j + 1; k <= '9'; k++)
 			{
 				if (i != j && j != k)
 				{
