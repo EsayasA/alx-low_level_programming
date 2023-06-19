1df318da8a46512c3dc2ab865a7f133d9d9c1f52
EsayasA <esayasaregawi29@gmail.com>
2023-06-19 09:42:35 -0700

    no11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10 ; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (i != j && j != k)
+				{
+				putchar('i');
+				putchar('j');
+				putchar('k');
+				}
+				if (i != 7)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}}}
+
+	return (0);
+}
