2bc8beb74e485fba26a52b5418bcf012fba9ad4f
EsayasA <esayasaregawi29@gmail.com>
2023-06-20 13:51:03 -0700

    no 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 -  prints all natural numbers from n to 98
+ * @n:input number
+ * Return:sum of natural number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (i != 98)
+		{
+		_putchar('i');
+		_putchar(',');
+		_putchar(' ');
+		}}
+	_putchar('\n');
+}
