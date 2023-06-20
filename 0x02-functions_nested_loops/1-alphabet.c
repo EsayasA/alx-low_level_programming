c5a9285b29796aaca52d3aa69db940df2430d317
EsayasA <esayasaregawi29@gmail.com>
2023-06-20 06:52:51 -0700

    no 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -print _putchar
+ * is written by Esayas
+ * Return:0
+ */
+int main(void)
+{
+int lett;
+for (lett = 'a'; lett <= 'z'; lett++)
+{
+	putchar(lett);
+}
+putchar('\n');
+return (0);
+}
