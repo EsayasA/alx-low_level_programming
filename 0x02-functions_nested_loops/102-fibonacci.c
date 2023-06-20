954ed7f44105bf11e34fd1cbd10f668eb9e957e6
EsayasA <esayasaregawi29@gmail.com>
2023-06-20 15:11:13 -0700

    no 12

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - fibonicial
+ * @void
+ * Return:series
+ */
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 2;
+	int sum = 0;
+
+	printf("%d", num1);
+	printf("%d", num2);
+	for (i = 0; i < 48; i++)
+	{
+		num1 = num2;
+		sum = num1 + num2;
+		num2 = sum;
+		printf("%d, ", sum);
+	}
+	printf("\n");
+	return (0);
+}
