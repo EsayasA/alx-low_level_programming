05929922b15582b96e51ff0d74c9bdd0c810e658
EsayasA <esayasaregawi29@gmail.com>
2023-06-20 14:29:02 -0700

    no 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,17 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
+	for (i = 0; i <= 98; i++)
+	{
+		if (n / 10)
+		{
+			print_to_98(n / 10);
+		}
 		if (i != 98)
 		{
-		_putchar('i');
+		_putchar(n % 10 + '0');
 		_putchar(',');
 		_putchar(' ');
 		}}
