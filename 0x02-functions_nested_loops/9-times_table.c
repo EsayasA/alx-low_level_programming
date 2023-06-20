6bbeff82bb6dfb9c4a10d8e844e9c8d1efaeb070
EsayasA <esayasaregawi29@gmail.com>
2023-06-20 14:10:15 -0700

    no 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,30 @@
  */
 void times_table(void)
 {
-	int i, j, z;
+	int i, j, z, u, d, y;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			z = i * j;
-			_putchar(z + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
+				_putchar(44)
+					_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}}
 		_putchar('\n');
 	}}
