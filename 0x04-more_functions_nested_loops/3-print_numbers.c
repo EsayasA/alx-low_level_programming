46c129a03050e3f1da5276f308a72d4c1ab79ab6
EsayasA <esayasaregawi29@gmail.com>
2023-06-22 13:42:37 -0700

    no 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,10 @@
 
 void print_numbers(void)
 {
-	char ch;
-
-	for (ch = '0'; ch <= '9'; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
 }
