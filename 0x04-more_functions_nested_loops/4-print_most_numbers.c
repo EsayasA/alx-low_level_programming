caead13d730cc531456b14980cde726c3a48def3
EsayasA <esayasaregawi29@gmail.com>
2023-06-22 13:44:27 -0700

    no 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	char ch;
-	for (ch = '0'; ch <= '9'; ch++)
-	{
-		if (ch != '2' && ch != '4')
-		{
-			_putchar(ch);
-		}
-	}
-	_putchar('\n');
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+if (ch != '2' && ch != '4')
+{
+_putchar(ch);
+}
+}
+_putchar('\n');
 }
