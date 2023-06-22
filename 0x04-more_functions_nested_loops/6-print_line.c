334ec69d70fee69db3bac7225c90c84ec68091db
EsayasA <esayasaregawi29@gmail.com>
2023-06-22 11:58:18 -0700

    no 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - that draws a straight line in the termina
+ * @n:number
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
