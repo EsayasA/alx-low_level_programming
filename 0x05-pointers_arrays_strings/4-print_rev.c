97815813a8cf6a5e409fe4e5449b4160eeaae2a7
EsayasA <esayasaregawi29@gmail.com>
2023-06-26 10:30:17 -0700

    no 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ * @s:input string
+ * Return:void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
