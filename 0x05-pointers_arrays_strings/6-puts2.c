34e1e0508c6460d21b5b3b1017a6e84325e9942d
EsayasA <esayasaregawi29@gmail.com>
2023-06-26 11:23:34 -0700

    no 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,15 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int j = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		i++;
+	}
+	for (j = 0; j <= i-1; j+=2)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
