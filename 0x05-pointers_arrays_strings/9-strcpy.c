037baf3bf80b38e645e4aa334ae42b2e139f5691
EsayasA <esayasaregawi29@gmail.com>
2023-06-26 12:53:53 -0700

    no 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - copies the string pointed to by
+ * @dest:will contain
+ * @src:sources file
+ * Return:pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+	return dest;
+}
