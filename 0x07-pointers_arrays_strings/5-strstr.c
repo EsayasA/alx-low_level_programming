e76653fd2480e7ca636fead6697e7bd81e887662
EsayasA <esayasaregawi29@gmail.com>
2023-07-03 12:52:16 -0700

    no 7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strstr - unction that locates a substring.
+ * @haystack:word
+ * @needle:needle
+ * Return:chaar
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
