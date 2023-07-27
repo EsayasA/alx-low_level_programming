6bd474bc95be35c8f63103e30fd220e65037c25a
EsayasA <esayasaregawi29@gmail.com>
2023-07-27 11:10:19 -0700

    no 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
- * list_len - number of elements in a linked 
- * @h:head
- * Return:number of elements
+ * list_len - returns the number of elements in a linked list
+ * @h: head
+ * Return: number of elements in head
  */
+
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
