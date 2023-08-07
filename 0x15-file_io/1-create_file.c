5a3aed26affe3b8b9006088ec5bbf7af79abf83c
EsayasA <esayasaregawi29@gmail.com>
2023-08-07 07:09:44 -0700

    no 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,9 @@ int create_file(const char *filename, char *text_content)
 			i++;
 	}
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write (file, text_content, i);
+	wr = write(file, text_content, i);
 	if (wr == -1 || file == -1)
 		return (-1);
-	close (file);
+	close(file);
 	return (1);
 }
