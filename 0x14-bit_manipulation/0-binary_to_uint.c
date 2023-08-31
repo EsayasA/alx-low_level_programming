265488f99679b6f8536dcd5886801418b4af5f86
EsayasA <esayasaregawi29@gmail.com>
2023-08-31 01:45:10 -0700

    no 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - changingin binary to decimal
+ * binary_to_uint - changing binary to decimal
  * @b:the given binary
  * Return: the decimal value.
  */
@@ -13,13 +13,11 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-
 	for (j = 0; b[j]; j++)
 	{
-		if (b[j] < '0' || b[j] > '1')
+		if ((b[j] < '0') || (b[j] > '1'))
 			return (0);
 		decimal = 2 * decimal + (b[j] - '0');
 	}
-
 	return (decimal);
 }
