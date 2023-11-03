573a61f3705466b9871c2fd923ffec77340bcbd5
EsayasA <esayasaregawi29@gmail.com>
2023-11-03 09:33:40 -0700

    hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,50 +1,53 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - update
- * @ht:pointer
- * @key:key
- * @value:value
- * Return:0 or 1
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *n;
-	char *copy;
-	unsigned long int indx, j;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	vcopy = strdup(value);
-	if (vcopy == NULL)
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
 
-	indx = key_index((const unsigned char *)key, ht->size);
-	for (j = indx; ht->array[j]; j++)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = vcopy;
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
 
-	n = malloc(sizeof(hash_node_t));
-	if (n == NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		free(vcopy);
+		free(value_copy);
 		return (0);
 	}
-	n->key = strdup(key);
-	if (n->key == NULL)
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
-		free(n);
+		free(new);
 		return (0);
 	}
-	new->value = vcopy;
-	n->next = ht->array[indx];
-	ht->array[indx] = n;
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
